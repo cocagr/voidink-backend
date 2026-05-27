b9d31421e8a3438fd2e40c3a7950eb9d5114333c
cocagr <joseignaciocastrococa@gmail.com>
2026-05-27 17:19:10 +0200

    fix major error

---
+++ src/main/java/org/voidink/api/VoidinkApplication.java
@@ -0,0 +1,13 @@
+package main.java.org.voidink.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoidinkApplication {
+
+    public static void main(String[] squadron) {
+        // Este método levanta el servidor embebido Tomcat en el puerto asignado
+        SpringApplication.run(VoidinkApplication.class, squadron);
+    }
+}
