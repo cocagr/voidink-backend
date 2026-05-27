6e19c36f8eb23f4badc12309c62ed226030a2ca2
cocagr <joseignaciocastrococa@gmail.com>
2026-05-27 18:20:03 +0200

    SecurityConfig

--- src/main/java/org/voidink/api/VoidinkApplication.java
+++ src/main/java/org/voidink/api/VoidinkApplication.java
@@ -1,4 +1,4 @@
-package main.java.org.voidink.api;
+package org.voidink.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
